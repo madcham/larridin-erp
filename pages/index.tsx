0ab5b259264d1a9a1a162b899454cd67adb0d473
madcham <madhu@hkmc.us>
2024-09-26 22:28:24 -0700

    initial larridin-ERP project setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Larridin ERP Integration</h1>
+      <p>Welcome to the new project!</p>
+    </div>
+  )
+}
+
+export default Home
