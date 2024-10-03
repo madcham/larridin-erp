10f79876da603cdd004a1ad593d29aa919d544c5
madcham <madhu@hkmc.us>
2024-10-03 00:16:53 -0700

    updated package.json to fix react version issues, and _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
