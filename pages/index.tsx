e221c8731e07dcaea6614627638433f6df4b3197
madcham <madhu@hkmc.us>
2024-10-04 10:58:18 -0700

    updated import settings in index tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import React, { useState } from 'react'
-import FutureModules from '@/components/ui/FutureModules'
-import DelegationSettings from '@/components/ui/DelegationSettings'
-import EmployeePortal from '@/components/ui/EmployeePortal'
+import {FutureModules} from '@/components/ui/FutureModules'
+import {DelegationSettings} from '@/components/ui/DelegationSettings'
+import {EmployeePortal} from '@/components/ui/EmployeePortal'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
