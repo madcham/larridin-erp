5269cca19e6dc29fa72facf063e5c02aec7706f0
madcham <madhu@hkmc.us>
2024-10-04 00:05:28 -0700

    updated futuremodules import syntax in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import FutureModules from '../components/FutureModules'
+import FutureModules from '@/components/ui/FutureModules'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
