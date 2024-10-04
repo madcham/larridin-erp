50904cc967edfc3f8f75b88a5b679e369083b7b7
madcham <madhu@hkmc.us>
2024-10-03 23:51:51 -0700

    updated index.tsx to correctly reference futuremodules import

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default Home */
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import FutureModules from './FutureModules'
+import FutureModules from "@/components/ui/FutureModules"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
